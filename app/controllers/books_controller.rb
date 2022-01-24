5f90e43d960a2255d04c09aad64e225e7d9a27d1
Miyawaki <tetsu.miyawaki@gmail.com>
2022-01-24 06:16:42 +0000

    [Update]Oyo-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+  def new
+    @book =Book.new
+  end
 
   def show
     @book = Book.find(params[:id])
@@ -6,14 +9,15 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
+    @books = Book.all
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
-      @books = Book.all
       render 'index'
     end
   end
