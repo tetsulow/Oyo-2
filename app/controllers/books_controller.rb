04b7beb00c4efc64bb4740048d016b6cfadf61c0
Miyawaki <tetsu.miyawaki@gmail.com>
2022-01-24 09:07:51 +0000

    [Finish]Oyo-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
@@ -44,6 +45,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,13 @@
 class User < ApplicationRecord
+  has_many :books, dependent: :destroy
+  has_one_attached :profile_image
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
-  has_one_attached :profile_image
-
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-
-
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
