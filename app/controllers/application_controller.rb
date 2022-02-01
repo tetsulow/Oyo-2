1097864bc16f5a8417c63d493a466f25b915c483
Miyawaki <tetsu.miyawaki@gmail.com>
2022-02-01 04:50:31 +0000

    [Finish]Oyo-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    root_path(current_user)
+    root_path
   end
 
   private
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -9,6 +9,7 @@ class RelationshipsController < ApplicationController
 		current_user.unfollow(params[:user_id])
 		redirect_to request.referer
 	end
+
   def followings
 		user = User.find(params[:user_id])
 		@users = user.followings
