0bd5683379c7b7b0a553954e8b05de46ec596d03
Miyawaki <tetsu.miyawaki@gmail.com>
2022-02-01 08:54:55 +0000

    [Update]Oyo-6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,12 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     @favorite = current_user.favorites.new(book_id: @book.id)
     @favorite.save
-    redirect_to user_path(@book.user)
    end
+
 
   def destroy
     @book = Book.find(params[:book_id])
     @favorite = current_user.favorites.find_by(book_id: @book.id)
     @favorite.destroy
-    redirect_to user_path(@book.user)
   end
 end
