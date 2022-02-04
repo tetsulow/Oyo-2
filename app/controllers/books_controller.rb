b1ec7422034bb6427ca18e8b9a8e86f3d0bdfcdb
Miyawaki <tetsu.miyawaki@gmail.com>
2022-02-04 03:13:58 +0000

    [Finish]Oyo-7

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,9 +12,9 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    @books = Book.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
     @book = Book.new
-    
+
   end
 
   def create
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,13 +3,14 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites ,dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-  
+
   def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
