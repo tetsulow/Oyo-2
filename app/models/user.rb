1620d640fb978874cf28bfab9672361b25e74a94
Miyawaki <tetsu.miyawaki@gmail.com>
2022-02-07 06:03:20 +0000

    [Update]Oyo-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :rooms, through: :user_rooms
   has_one_attached :profile_image
 
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
